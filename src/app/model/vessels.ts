638f25c679a89cc32f3597b6d40befae512f346d
lsagona <loic.sagona@edu.univ-fcomte.fr>
2020-11-18 09:46:55 +0100

    determine vessel date of first appearance

--- src/app/model/vessels.ts
+++ src/app/model/vessels.ts
@@ -3,6 +3,7 @@ import {Message} from './message';
 
 export class Vessels {
   vessels: Map<number, Vessel>;
+  firstAppearance: number;
 
   constructor() {
     this.vessels = new Map<number, Vessel>();
@@ -12,8 +13,15 @@ export class Vessels {
     if (!this.vessels.get(Number(message.mmsi))) {
       this.vessels.set(Number(message.mmsi), new Vessel(new Array<Message>()));
     }
-
+    this.determineFirstAppearance(message);
     this.vessels.get(Number(message.mmsi)).addMessage(message);
+  }
+
+  determineFirstAppearance(message: Message): void {
+    const timeInS = Date.parse(message.time) / 1000;
+    if (this.firstAppearance > timeInS) {
+      this.firstAppearance = timeInS;
+    }
   }
 
 }
