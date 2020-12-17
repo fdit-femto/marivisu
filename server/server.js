d85a492dc06b39d0789295a66fe7ea0d1ee529bd
lsagona <loic.sagona@edu.univ-fcomte.fr>
2020-12-17 14:33:34 +0100

    server: don't start right away and wait for angular call to fetch data from FDI-T

--- server/server.js
+++ server/server.js
@@ -1,50 +1,53 @@
 const express = require('express')
 const cors = require('cors');
 const net = require('net');
+const bodyParser = require('body-parser');
+
+
 const client = new net.Socket();
 const server = express()
 
 server.use(cors())
+server.use(bodyParser.json());
 const port = 3800
 let vesselsData = '';
-
-server.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-server.post('/client/startClient', (req, res) => {
-  console.log(`Client started.`)
-  const client = req.body.client
-  startClient(client)
-});
 
 server.listen(port, () => {
   console.log(`Server app listening at http://localhost:${port}`)
 })
 
-server.get('/client/getmessages', (request, response) => {
-  response.json(vesselsData)
+server.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-client.connect(1024, "127.0.0.1", function() {
-  console.log('Connected');
+server.post('/client/startClient', (request, response) => {
+  console.log(`Starting client.`)
+  const client = request.body
+  startClient(client)
 });
 
-client.on('data', function(data) {
-  vesselsData += data
-  console.log('Received: ' + data + '\n---------\n');
-  console.log(vesselsData);
-});
+server.get('/client/getMessages', (request, response) => {
+  console.log(`getMessages.`)
+
+  let vesselsToSend = vesselsData
+  response.json(vesselsToSend)
+  vesselsData = diff(vesselsToSend, vesselsData)
+})
 
 
 function startClient(clientModel) {
-  client.connect(clientModel.port, clientModel.ip, function() {
-    console.log('Connected');
+  console.log('coucou')
+  client.connect(clientModel.port, clientModel.ip, function () {
+    console.log('Connected to ' + clientModel.ip + ':' + clientModel.port)
   });
 
-  client.on('data', function(data) {
-    vesselsData += data
-    console.log('Received: ' + data + '\n---------\n');
+  client.on('data', function (data) {
+    vesselsData += data;
+    console.log('Received: ' + data);
     console.log(vesselsData);
   });
+}
+
+function diff(strToRemove, str) {
+  return str.split(strToRemove).join('')
 }
