8173863681713066fb82b2f30aea8c4af7bb992a
lsagona <loic.sagona@edu.univ-fcomte.fr>
2021-01-18 11:53:59 +0100

    add slider min max

--- src/app/model/vessels.ts
+++ src/app/model/vessels.ts
@@ -3,16 +3,25 @@ import {Message} from './message';
 
 export class Vessels {
   vessels: Map<number, Vessel>;
+  firstAppearance: number;
+  lastAppearance: number;
 
   constructor() {
     this.vessels = new Map<number, Vessel>();
   }
 
   addMessage(message: Message): void {
+    this.determineFirstAppearance(message);
+    this.determineLastAppearance(message);
     if (!this.vessels.get(Number(message.mmsi))) {
       this.vessels.set(Number(message.mmsi), new Vessel(new Array<Message>()));
+      this.determineFirstAppearance(message);
     }
     this.vessels.get(Number(message.mmsi)).addMessage(message);
+  }
+
+  addVessel(vessel: Vessel): void {
+    this.vessels.set(Number(vessel.getMMSI()), vessel);
   }
 
   sortAllMessageInVesselByDate(): void {
