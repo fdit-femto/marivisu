d96283e25683cb8e2b7f9e0bcb25544f09af0a82
lsagona <loic.sagona@edu.univ-fcomte.fr>
2021-02-24 15:13:44 +0100

    add replay button

--- src/app/component/app.module.ts
+++ src/app/component/app.module.ts
@@ -22,6 +22,8 @@ import { SliderComponent } from './slider/slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 PlotlyModule.plotlyjs = PlotlyJS;
 
@@ -49,7 +51,9 @@ PlotlyModule.plotlyjs = PlotlyJS;
     HttpClientModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     {provide: 'SnotifyToastConfig', useValue: ToastDefaults},
