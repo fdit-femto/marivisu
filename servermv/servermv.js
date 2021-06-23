8f6236cde22e6a17b27f51258a4f0be767241983
lsagona <loic.sagona@edu.univ-fcomte.fr>
2021-06-23 14:39:37 +0200

    server : connection to elasticsearch

--- servermv/servermv.js
+++ servermv/servermv.js
@@ -4,13 +4,17 @@ const express = require('express')
 const cors = require('cors');
 const net = require('net');
 const bodyParser = require('body-parser');
-
+const elasticsearch = require('elasticsearch');
 const server = express()
 
 const labelType = {
   NONE: 0,
   ASD: 1
 }
+
+const elasticsearchClient = new elasticsearch.Client({
+  node: 'https://localhost:9200'
+})
 
 const parseRawBody = (req, res, next) => {
   req.setEncoding('utf8');
@@ -31,7 +35,83 @@ let dataVessels = new Map();
 let newDataVessels = new Map();
 let labels = new Map();
 
+async function esDelete() {
+  await elasticsearchClient.indices.delete({index: 'ais'})
+}
+
+async function esCreateIndices() {
+  await elasticsearchClient.indices.create({index: 'ais'})
+}
+
+async function esInit() {
+  await elasticsearchClient.indices.exists({index: 'ais'}, (err, res, status) => {
+    if (res) {
+      esDelete()
+        .then(res => {
+          console.log("[ES] delete");
+        })
+        .catch(err => console.log('[ERROR] : ' + err))
+    }
+    esCreateIndices().then(_ => {
+      console.log('[ES] create index')
+      esPutMapping()
+        .then(_ => {
+          console.log('[ES] create mapping')
+        })
+        .catch(err => console.log('[ERROR] : ' + err))
+    })
+  })
+}
+
+function createPositionField(data) {
+  data.position = {
+    lat: data["LAT"],
+    lon: data["LON"]
+  }
+
+  return data
+}
+
+async function esPutMapping() {
+  await elasticsearchClient.indices.putMapping({
+    index: 'ais',
+    body: {
+      properties: {
+        "MMSI": {"type": "integer"},
+        "timestamp": {"type": "date"},
+        "position": {"type": "geo_point"},
+        "SOG": {"type": "integer"},
+        "label": {"type": "text"}
+      }
+    }
+  });
+}
+
+async function bulk(data) {
+  const body = data.flatMap(doc => [{index: {_index: 'ais', _id: doc.MMSI}}, createPositionField(doc)])
+  await elasticsearchClient.bulk({refresh: true, body}, function (err, resp) {
+    if (resp.errors) {
+      const erroredDocuments = []
+      resp.items.forEach((action, i) => {
+        const operation = Object.keys(action)[0]
+        if (action[operation].error) {
+          erroredDocuments.push({
+            status: action[operation].status,
+            error: action[operation].error,
+            operation: body[i * 2],
+            document: body[i * 2 + 1]
+          })
+        }
+      })
+      console.log(erroredDocuments)
+    }
+  })
+
+}
+
+
 server.listen(port, () => {
+  esInit().then(res => console.log(res)).catch(err => console.log(err))
   console.log(`Data server listening at http://localhost:${port}`)
 })
 
@@ -40,28 +120,39 @@ server.get('/', (request, response) => {
 })
 
 /**
- * Receive data vessel.
+ * ElasticSearch
  */
 server.post('/api/send_data', (request, response) => {
   let dataReceived = JSON.parse(JSON.parse(request.rawBody));
-  dataReceived.forEach(element => {
-    if (!dataVessels.has(element.MMSI)) {
-      const vessel = new SVessel();
-      vessel.addMessage(element);
-      dataVessels.set(element.MMSI, vessel);
-    } else {
-      dataVessels.get(element.MMSI).addMessage(element);
-    }
-    if (!newDataVessels.has(element.MMSI)) {
-      const vessel = new SVessel();
-      vessel.addMessage(element);
-      newDataVessels.set(element.MMSI, vessel);
-    } else {
-      newDataVessels.get(element.MMSI).addMessage(element);
-    }
-  })
+  bulk(dataReceived).then(res => {
+    console.log(res)
+  }).catch(err => console.log(err))
   response.sendStatus(200);
 });
+
+/**
+ * Receive data vessel.
+ */
+// server.post('/api/send_data', (request, response) => {
+//   let dataReceived = JSON.parse(JSON.parse(request.rawBody));
+//   dataReceived.forEach(element => {
+//     if (!dataVessels.has(element.MMSI)) {
+//       const vessel = new SVessel();
+//       vessel.addMessage(element);
+//       dataVessels.set(element.MMSI, vessel);
+//     } else {
+//       dataVessels.get(element.MMSI).addMessage(element);
+//     }
+//     if (!newDataVessels.has(element.MMSI)) {
+//       const vessel = new SVessel();
+//       vessel.addMessage(element);
+//       newDataVessels.set(element.MMSI, vessel);
+//     } else {
+//       newDataVessels.get(element.MMSI).addMessage(element);
+//     }
+//   })
+//   response.sendStatus(200);
+// });
 
 /**
  * Receive label vessel.
